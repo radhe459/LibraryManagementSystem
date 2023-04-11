949819b2af1e523ec177ab079fb4396aef7c7718
Radhe Shyam Chaubey <informativersc1234@gmail.com>
2023-04-11 12:08:03 +0530

    recieving added

--- js/books.js
+++ js/books.js
@@ -1,11 +1,24 @@
-let btn_list = document.getElementsByClassName("gtbut")
-for (i = 0; i < btn_list.length; i++) {
-    btn_list[i].setAttribute("id", "c" + i);
+let get_list = document.getElementsByClassName("gtbut")
+for (i = 0; i < get_list.length; i++) {
+    get_list[i].setAttribute("id", "c" + i);
+}
+let return_list = document.getElementsByClassName("rtbut")
+for (i = 0; i < return_list.length; i++) {
+    return_list[i].setAttribute("id", "cr" + i);
 }
 
 function getIt(click_id) {
     const click_btn = document.getElementById(click_id)
     const click_span = click_btn.parentElement.previousElementSibling.firstElementChild
     let count = click_span.innerHTML;
-    click_span.innerHTML = Number.parseInt(count) - 1;
+    if (click_span.innerHTML > 0)
+        click_span.innerHTML = Number.parseInt(count) - 1;
+}
+
+function returnIt(click_id) {
+    const click_btn = document.getElementById(click_id)
+    const click_span = click_btn.parentElement.previousElementSibling.firstElementChild
+    let count = click_span.innerHTML;
+    if (click_span.innerHTML < 100)
+        click_span.innerHTML = Number.parseInt(count) + 1;
 }
